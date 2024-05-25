736c20273de4e6ae03741090b36cbd19f10cf6af
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-05-26 00:52:59 +0200

    chore: Refactor app.py for better code organization and readability

--- ai_planner/src/app.py
+++ ai_planner/src/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, jsonify
-
-# from calendar_interface import CalendarInterface
-from ollama_interface import OllamaInterface
-import signal
 import os
+import signal
 from logging import getLogger
+
+from calendar_interface import CalendarInterface
+from flask import Flask, jsonify, render_template, request
+from ollama_interface import OllamaInterface
 
 logger = getLogger(__name__)
 app = Flask(__name__)
+event = None
 
 # Initialize the conversation with the first bot message
 conversations = [
@@ -30,11 +31,18 @@ def index():
 def send_message():
     user_message = request.form["message"]
     # Add user message to conversation
-    logger.warning(f"User message: {user_message}")
+    logger.info(f"User message: {user_message}")
     conversations.append({"sender": "user", "message": user_message})
 
-    bot_response = ollama_interface.chat(user_message)
-    logger.warning(f"Bot response: {bot_response}")
+    global event
+    response = ollama_interface.chat(user_message)
+    if response:
+        event, bot_response = ollama_interface.refine_message(response)
+    else:
+        event = None
+        bot_response = "I'm sorry, I didn't understand that. Could you please rephrase?"
+
+    # logger.info(f"Bot response: {bot_response}")
     conversations.append({"sender": "bot", "message": bot_response})
 
     # if the user typed quit then the chat will be closed
@@ -45,6 +53,43 @@ def send_message():
                 "message": "Thank you for using the calendar assistant. Have a great day!",
             }
         )
+
+    return jsonify(conversations)
+
+
+@app.route("/add_event", methods=["POST"])
+def add_event():
+    global event
+    if event:
+        # add event to the calendar
+        calendar_interface = CalendarInterface()
+        # remove spaces from event keys and make only first letter lowercase
+        event = {k.replace(" ", "").lower(): v for k, v in event.items()}
+        normalised_event = {
+            "summary": event["summary"],
+            "description": event["description"],
+            "location": event["location"],
+            "start": {
+                "dateTime": event["startdatetime"],
+                "timeZone": (
+                    event["timezone"] if "timezone" in event else "Europe/Berlin"
+                ),
+            },
+            "end": {
+                "dateTime": event["enddatetime"],
+                "timeZone": (
+                    event["timezone"] if "timezone" in event else "Europe/Berlin"
+                ),
+            },
+        }
+
+        # drops keys with None values or ""
+        normalised_event = {k: v for k, v in normalised_event.items() if v}
+        logger.info(f"Normalised event: {normalised_event}")
+        calendar_interface.add_event(normalised_event)
+        logger.info("Event added to the calendar.")
+    else:
+        logger.info("No event data available to add to the calendar.")
 
     return jsonify(conversations)
 
