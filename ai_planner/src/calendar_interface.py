e5cfc3a5bfda394206c30a4c7e2c72134729488e
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-05-26 00:53:05 +0200

    refactor: Refine OllamaInterface message extraction method

--- ai_planner/src/calendar_interface.py
+++ ai_planner/src/calendar_interface.py
@@ -1,13 +1,12 @@
 import os.path
 
+from config import SCOPES
+from exceptions import NoCredentialsError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
-from exceptions import NoCredentialsError
-from config import SCOPES
 
 
 class CalendarInterface:
--- ai_planner/src/ollama_interface.py
+++ ai_planner/src/ollama_interface.py
@@ -1,5 +1,6 @@
-import ollama
 import ast
+
+import ollama
 
 
 class OllamaInterface:
@@ -32,7 +33,6 @@ class OllamaInterface:
         Method to refine the user message before showing it in the UI.
         It seperates extracts the json object from the bot message string to add it to calcender.
         The method cuts content between { and } if they exist.
-        it also removes 3 lines before first { and 1 lines after last }.
 
         it returns the json object extracted from the message and the rest of the message.
         Args:
@@ -45,42 +45,8 @@ class OllamaInterface:
         str
             The refined message
         """
+        if "{" not in user_message:
+            return None, user_message
         start = user_message.find("{")
         end = user_message.rfind("}")
-        return (
-            ast.literal_eval(user_message[start : end + 1]),
-            user_message[: start - 1] + user_message[end + 2 :],
-        )
-
-
-if __name__ == "__main__":
-    interface = OllamaInterface()
-    print(
-        interface.refine_message(
-            """
-                             I extracted the following fields from your text:
-
-**Summary:** Dentist Appointment
-**Description:** Fixing teeth
-**Location:** Berlin (assuming EU/Berlin time zone)
-**Start Date and Time:** 2024-05-25T09:00:00
-**End Date and Time:** Not provided (assuming the appointment ends at some point, but I'll need you to confirm this)
-
-Please let me know if this is correct or if you'd like to add any other details. Also, please confirm your End Date and Time.
-
-Here's a summary of what we have so far:
-
-```
-{
-  "Summary": "Dentist Appointment",
-  "Description": "Fixing teeth",
-  "Location": "Berlin",
-  "Start Date and Time": "2024-05-25T09:00:00",
-  "End Date and Time": "?",
-  "Time Zone": "EU/Berlin"
-}
-```
-
-Would you like to add more events or stop here?"""
-        )[0]
-    )
+        return (ast.literal_eval(user_message[start : end + 1]), user_message)
