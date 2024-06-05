11a86d652df6c1b64badef58b310d36a5331f1f1
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-06-06 01:56:13 +0200

    refactor: Update app.py to use consistent quotation marks for host and port in app.run()

--- ai_planner/src/app.py
+++ ai_planner/src/app.py
@@ -98,4 +98,4 @@ def quit():
 
 # I have a dentist appointment on 4th of June 2024 of 10:00 AM for 1 hour in Munich.
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host="0.0.0.0", port=5050)
